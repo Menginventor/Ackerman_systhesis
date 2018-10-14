5cf43c7efaabb3ac42beb56081b5410150c91cab
Menginventor <meng.inventor@gmail.com>
2018-10-15 06:36:57 +0700

    can draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore, QtGui
-from PyQt5.QtGui import QPainter
+from PyQt5.QtGui import QPainter, QColor, QPen
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtCore import QSettings
 import sys
@@ -43,7 +43,7 @@ class main_tab(QTabWidget):
         self.addTab(self.tab2, "Tab 2")
         self.addTab(self.tab3, "Tab 3")
     def Chassis_setup_tab_UI (self):
-        main_layout = QHBoxLayout()
+        main_layout = QVBoxLayout()
 
         GB1 = QGroupBox("Wheel Distance")
         layout = QFormLayout()
@@ -58,9 +58,66 @@ class main_tab(QTabWidget):
         layout.addRow(QLabel("Kingpin distance"), LE3)
         GB1.setLayout(layout)
         main_layout.addWidget(GB1)
-        #Chassis_drawing = Example()
-        #main_layout.addWidget(Chassis_drawing)
+        #Chassis_drawing_widget = Chassis_drawing(QColor(0, 0, 0))
+        Chassis_drawing_widget = gameWindow()
+
+        main_layout.addWidget(Chassis_drawing_widget)
         self.Chassis_setup_tab.setLayout(main_layout)
+class gameWindow(QtGui.QWidget):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+
+        self.setGeometry(300,300,1280,800)
+
+        self.frame = QtGui.QFrame()
+        self.frame.setFixedSize(500, 500)
+        #self.frame.setStyleSheet("background-color: rgb(200, 255, 255)")
+        self.frame.setFrameStyle(QFrame.Panel );
+        self.frame.setLineWidth(1)
+        layout=QtGui.QVBoxLayout()
+        layout.addWidget(self.frame)
+        self.setLayout(layout)
+        self.color = QColor(0, 0, 0)
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+
+        # painter.setRenderHint(QtGui.QPainter.Antialiasing)
+        radx = 5
+        rady = radx
+        if self.color in ('red', 'green'):
+            col = QtGui.QColor(self.color)
+            painter.setPen(col)
+            painter.setBrush(col)
+        k = int(radx * 1.5)
+        center = QtCore.QPoint(250, 250)
+        painter.drawEllipse(center, radx, rady)
+
+class Chassis_drawing(QtGui.QWidget):
+    def __init__(self, color , parent=None):
+        QtGui.QWidget.__init__(self, parent)
+
+        self.color = color
+
+
+    def setColor(self,color):
+        self.color = color
+        self.update()
+
+    def paintEvent(self,event):
+        painter = QtGui.QPainter(self)
+
+        #painter.setRenderHint(QtGui.QPainter.Antialiasing)
+        radx = 5
+        rady = radx
+        if self.color in ('red', 'green'):
+            col = QtGui.QColor(self.color)
+            painter.setPen(col)
+            painter.setBrush(col)
+        k = int(radx * 1.5)
+        center = QtCore.QPoint(100,200)
+        painter.drawEllipse(center,radx,rady)
+
 
 
 class main_widget(QWidget):
